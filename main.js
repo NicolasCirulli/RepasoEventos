21fa4be1b4ebdb81ef3483df3aa7d72ed6f46170
NicolasCirulli <nicolascirulli2@gmail.com>
2023-02-06 14:46:42 -0300

    .

---
+++ main.js
@@ -0,0 +1,105 @@
+const $container = document.getElementById('card-container')
+const $select = document.getElementById( 'select' )
+const $radioContainer = document.getElementById('radio-container')
+
+const datos = personajes.data.filter( personaje => personaje.isAvailableForTest )
+
+const rolesRepetidosArray = datos.map( personaje => personaje.role.displayName )
+
+const rolesSinRepetidosSet = new Set( rolesRepetidosArray )
+
+const rolesSinRepetidosArray = [ ...rolesSinRepetidosSet ]
+
+
+
+agregarCard( datos, $container )
+agregarOption( rolesSinRepetidosArray, $select )
+
+
+// eventos
+
+$radioContainer.addEventListener( 'change', (e) => {
+    const filtrados = filtrarPersonajesRadio( datos, e.target.value )
+    const filtradosPorSelect = filtrarPersonajesSelect( filtrados, $select.value )
+    agregarCard( filtradosPorSelect, $container )
+   
+})
+
+$select.addEventListener( 'change', (e) =>{
+    const radioChecked = document.querySelector('input[type="radio"]:checked')
+    const filtrados = filtrarPersonajesSelect( datos, $select.value )
+    const filtradosRadio = filtrarPersonajesRadio( filtrados, radioChecked.value )
+    agregarCard( filtradosRadio, $container )
+})
+
+
+// funciones
+
+function agregarCard(lista, elemento){
+    elemento.innerHTML = ''
+    let template = ''
+    for( let personaje of lista ){
+        template += crearCard(personaje)
+    }
+    elemento.innerHTML += template
+}
+
+function crearCard( personaje ){
+    return  `
+    <div class="card col-9 col-md-3">
+        <img class="card-img-top" src="${personaje.fullPortraitV2}" alt="Title">
+        <div class="card-body">
+            <h4 class="card-title text-center">${personaje.displayName}</h4>
+            <p class="card-text text-center">${personaje.description}</p>
+        </div>
+    </div>
+`
+}
+
+function agregarOption(lista, elemento){
+    let fragment = document.createDocumentFragment()
+    lista.forEach( rol => fragment.appendChild( crearOption(rol) ) )
+    elemento.appendChild(fragment)
+}
+
+function crearOption( rol ){
+
+    let option = document.createElement('option')
+    option.value = rol
+    option.textContent = rol
+    return option
+
+}
+
+function filtrarPersonajesSelect( personajes, value ){
+    if( value == 'all' ) return personajes
+    return personajes.filter( personaje => personaje.role.displayName == value ) 
+}   
+
+function filtrarPersonajesRadio( personajes, value){
+    if( value == 'all'){
+        return personajes
+    }
+    let aux = personajes.filter( personaje => {
+        if( value == 'base' ){
+            return personaje.isBaseContent
+        }
+        if( value == 'Contracts' ){
+            return !personaje.isBaseContent
+        }
+    } )
+
+    return aux
+}
+
+
+/* function agregarOptionInner(lista, elemento){
+    lista.forEach( rol => elemento.innerHTML += crearOptionInner(rol) )
+}
+
+function crearOptionInner( rol ){
+    return `<option value=${rol}>${rol}</option>`
+}
+ */
+
+
